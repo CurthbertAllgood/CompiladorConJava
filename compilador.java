e11ea983ae8c37c427ef5c1aeeda48723cd73608
Curthbert <carlosortiz.caceres2202@gmail.com>
2024-09-12 17:30:20 -0300

    se corrigio y se probo con todos los PL0 el analizador lexico, se sigue trabajando en el sintactico

--- compilador.java
+++ compilador.java
@@ -1,10 +1,14 @@
 package compilador;
 
+import compilador.tokens.ETerminal;
+import compilador.tokens.Token;
+
 import java.io.IOException;
 
 public class compilador {
 
     public static void main(String[] args) {
+        // Ruta del archivo a analizar
         String archivo = "/home/carlos-ortiz/Documentos/Sistemas De Computacion 1/compilador/BIEN-00.PL0";
         AnalizadorLexico a = new AnalizadorLexico(archivo);
         AnalizadorSintactico sintactico = new AnalizadorSintactico(a);
@@ -12,13 +16,40 @@ public class compilador {
         try {
             // Iniciar el análisis sintáctico llamando a `programa`
             sintactico.programa();
-
             System.out.println("Análisis sintáctico completado exitosamente.");
-            a.cerrar();
         } catch (IOException e) {
             System.out.println("Error durante el análisis: " + e.getMessage());
         } catch (Exception e) {
             System.out.println("Error sintáctico: " + e.getMessage());
+        } finally {
+            try {
+                a.cerrar();
+            } catch (IOException e) {
+                System.out.println("Error al cerrar el archivo: " + e.getMessage());
+            }
         }
+
+        // BUCLE ANALIZADOR LÉXICO (comentado)
+        /*
+        try {
+            Token token;
+            do {
+                token = a.scanear();
+                System.out.println(token);
+            } while (token.getTipo() != ETerminal.EOF);
+
+            System.out.println("Análisis léxico completado exitosamente.");
+        } catch (IOException e) {
+            System.out.println("Error durante el análisis: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error sintáctico: " + e.getMessage());
+        } finally {
+            try {
+                a.cerrar();
+            } catch (IOException e) {
+                System.out.println("Error al cerrar el archivo: " + e.getMessage());
+            }
+        }
+        */
     }
 }
